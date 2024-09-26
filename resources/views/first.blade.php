f06dac297d2317923a1964e4cf1545db3ad609fd
Dovidlo <davidkhairullin144@gmail.com>
2024-09-26 12:06:01 +0500

    qwwert

---
+++ resources/views/first.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+@section('content')
+    <h1>Первая страница</h1>
+    <p>Lorem ipsum distinctio, blanditiis ea cumque culpa omnis hic officia impedit. Eveniet, 
+    animi esse?</p>
+    
+    <img src="/imgs/bird.jpg" alt="">
+    <p class="qwerty">Ему {{$sum}} лет</p>
+@endsection()
