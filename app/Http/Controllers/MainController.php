cce9d12ee45a14cffd1c40919b2fb768b29bb7da
David Khairullin <david228223@gmail.com>
2024-10-07 21:21:27 +0500

    task2_FULL

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -21,11 +21,12 @@ class MainController extends Controller
 
     public function showArray(){
         $array = [
-            ['id' => 1, 'title' => 'продукт 1', 'price' => 500, 'path' => 'img/img1.jpg'],
-            ['id' => 2, 'title' => 'продукт 2', 'price' => 600, 'path' => 'img/img1.jpg'],
-            ['id' => 3, 'title' => 'продукт 3', 'price' => 700, 'path' => 'img/img1.jpg'],
-            ['id' => 4, 'title' => 'продукт 4', 'price' => 800, 'path' => 'img/img1.jpg'],
-            ['id' => 5, 'title' => 'продукт 5', 'price' => 900, 'path' => 'img/img1.jpg']
+            ['id' => 1, 'title' => 'продукт 1', 'price' => 400, 'path' => 'imgs/img1.jpg'],
+            ['id' => 2, 'title' => 'продукт 2', 'price' => 500, 'path' => 'imgs/img1.jpg'],
+            ['id' => 3, 'title' => 'продукт 3', 'price' => 600, 'path' => 'imgs/img1.jpg'],
+            ['id' => 4, 'title' => 'продукт 4', 'price' => 700, 'path' => 'imgs/img1.jpg'],
+            ['id' => 5, 'title' => 'продукт 5', 'price' => 800, 'path' => 'imgs/img1.jpg'],
+            ['id' => 6, 'title' => 'продукт 6', 'price' => 900, 'path' => 'imgs/img1.jpg']
         ];
         return view('array', ['array' => $array]);        
     }
--- resources/views/array.blade.php
+++ resources/views/array.blade.php
@@ -1,7 +1,25 @@
-@extends('layouts.main')
-@section('content')
-    @foreach($array as $arr)
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href='../css/style.css'>
+    <title>Document</title>
+</head>
+<body>
+    @extends('layouts.main')
+    @section('content')
+        <div class="cards">
+            @foreach($array as $arr)
+                <div class="card">
+                    <h2>{{ $arr['title'] }}</h2>
+                    <img class="card-img" src="{{ $arr['path'] }}" alt="{{ $arr['title'] }}">
+                    <p class='card-price'>Цена: {{ $arr['price'] }} рублей</p>
+                </div>
+                
+            @endforeach
+        </div>
         
-        <p>{{ $arr['title'] }}</p>
-    @endforeach
-@endsection()
+    @endsection()
+</body>
+</html>
